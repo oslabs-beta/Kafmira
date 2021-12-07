41a9e7688c706cbe303b2eef2207ed33e2aed99d
Gibran Haq <gibran.haq57@gmail.com>
2021-12-07 13:21:51 -0800

    boilerplate complete - pending react dev tools

--- main.js
+++ main.js
@@ -30,15 +30,15 @@ function createWindow() {
   });
 
 //new code here to add react dev tools/redux dev tools if we're in dev mode (using electron is dev)
-if (isDev) {
-  installExtension(REACT_DEVELOPER_TOOLS)
-   .then((name) => console.log(`Added Extension:  ${name}`))
-   .catch((err) => console.log('An error occurred: ', err));
+  if (isDev) {
+    installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
 
-  installExtension(REDUX_DEVTOOLS)
-   .then((name) => console.log(`Added Extension:  ${name}`))
-   .catch((err) => console.log('An error occurred: ', err));
-}
+    installExtension(REDUX_DEVTOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+  }
 
   //serve the index.html upon window load
   win.loadFile("index.html");
