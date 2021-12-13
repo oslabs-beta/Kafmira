1f57da81bd8f8d356f29f30f480255daa9fa7b10
nicholassuzuki <nicholassuzuki@yahoo.com>
2021-12-13 17:05:11 -0500

    logo component added

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import BarChart from '../components/BarChart';
+import GetLogo from '../components/GetLogo';
 
 const App =() => {
   return (
   
     <div>
+      
       <div>This is a react component DAWG</div>
       <div>Further testing of dev tools</div>
-      <div><BarChart /></div>
+      <div><GetLogo /></div>
       <Routes>
         
       </Routes>
