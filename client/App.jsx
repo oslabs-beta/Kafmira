f588611125c6070451888c05e1b5e5ef671b1e9c
Robert <robert.koulakes@gmail.com>
2021-12-13 14:25:13 -0800

    Completed port entry component

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import BarChart from '../components/BarChart';
+import BarChart from './components/BarChart';
+import PortEntry from './components/PortEntry';
 
 const App =() => {
   return (
   
     <div>
-      <div>This is a react component DAWG</div>
-      <div>Further testing of dev tools</div>
       <div><BarChart /></div>
+      <div><PortEntry /></div>
       <Routes>
         
       </Routes>
