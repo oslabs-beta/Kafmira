c6c8a1dde71dd63280cd9432d388ec0079d31e68
Gibran Haq <gibran.haq57@gmail.com>
2021-12-13 11:29:24 -0800

    Material UI modules/Fonts added, MainPageContainer, TopBannerContainer, and MainHeader components started

---
+++ client/components/MainPageContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainPageContainer =() => {
+  return (
+    <div>
+      <TopBannerContainer />
+      <ClusterContainer />
+      <CriticalMetrics />
+      <BottomBanner />
+    </div>
+  )
+}
+
+export default MainPageContainer;
