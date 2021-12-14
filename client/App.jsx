307610c04c6e571a2d4c53bf9cdb99c2e2b1a4b3
Gibran Haq <gibran.haq57@gmail.com>
2021-12-13 16:45:30 -0800

    App Bar working, hamburger menu adisplaying, logo placed

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import BarChart from './components/BarChart';
 import PortEntry from './components/PortEntry.jsx';
 import GetLogo from './components/GetLogo.jsx';
 import MainPageContainer from './components/MainPageContainer.jsx';
@@ -9,7 +8,6 @@ const App =() => {
   return (
   
     <div>
-      <div><BarChart /></div>
       <div><GetLogo /></div>
       <div><PortEntry /></div>
       <Routes>
