a64da843a18633b5bc91c60a3e1cae2bb323793a
nicholassuzuki <nicholassuzuki@yahoo.com>
2021-12-15 18:21:07 -0500

    trying to merge

--- client/components/BrokerFirstChart.jsx
+++ client/components/BrokerFirstChart.jsx
@@ -2,31 +2,12 @@ import React, { useState, useEffect }from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import LineChart from './LineChart.jsx'
 import {Grid, Paper} from '@material-ui/core';
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: 'column',
-    flexGrow: "1",
-    alignItems: "center",
-  },
-  title: {
-    color: "rgba(0, 0, 0, 0.54)",
-    fontSize: "42px",
-    fontWeight: "bold",
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    minHeight: "180px"
-  },
-}));
+
 
 const MakeBrokerCharts = () => {
   // uses styling above
-  const classes = useStyles()
-  const [subData, setSubData] = useState([]);
-  const [finalData, setFinalData] =useState([]);
+  // const classes = useStyles()
+ 
   
   const yAxis = [10,20,30,40];
   const xAxis = [0,1,2,3,4,5];
