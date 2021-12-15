3b52b2ef1cf5c82b4301e5e2f5a47573bc587e6f
Robert <robert.koulakes@gmail.com>
2021-12-15 14:26:57 -0800

    Completed graph containers for producer and consumer pages

--- client/components/ProducersPage.jsx
+++ client/components/ProducersPage.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import TopBannerContainerProdPage from './TopBannerContainerProdPage.jsx';
+import ProducerGraphContainer from './ProducerGraphContainer.jsx';
 
 export default function ProducersPage(){
   return(
+  <div>
   <TopBannerContainerProdPage/>
+  <ProducerGraphContainer />
+  </div>
   )
 }
