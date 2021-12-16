e2aa3526596fe7567c679b27cf4aa84f152546e5
nicholaos_sofocleous <nsofocleous92@gmail.com>
2021-12-16 16:25:31 -0500

    Consumer Bar and Line Chart Files added to ConsumerGraphContainer

--- client/components/ConsumerGraphContainer.jsx
+++ client/components/ConsumerGraphContainer.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Box, Grid } from '@material-ui/core';
+import ConsumerBarDummyData from './ConsumerBarChart.jsx'
+import ConsumerLineDummyData from './ConsumerLineChart.jsx'
 
 export default function ConsumerGraphContainer(){
   return(
@@ -12,11 +14,11 @@ export default function ConsumerGraphContainer(){
     >
       <Grid item xs={4}>
         {/* <Box border="1px solid black">Sample Graph Area 1</Box> */}
-        
+        <ConsumerBarDummyData/>
       </Grid>
       <Grid item xs={4}>
         {/* <Box border="1px solid black">Sample Graph Area 2</Box> */}
-
+        <ConsumerLineDummyData/>
       </Grid>
     </Grid>
   )
