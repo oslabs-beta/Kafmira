9e779e66d562c87fe28fa10b094ae55717578257
Gibran Haq <gibran.haq57@gmail.com>
2021-12-24 14:18:28 -0800

    Broker dynamically generated and displayed, port is currently hhardcoded, need to utilize redux port

--- client/components/Broker.jsx
+++ client/components/Broker.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router';
 import { Button, Grid } from '@material-ui/core';
 import { Stack } from '@mui/material';
 
-export default function Producer(){
+export default function Broker(){
   return(
     <Stack spacing={2}>
     <Button
@@ -12,7 +12,7 @@ export default function Producer(){
     >
     <p style={{ color: "white", textalign: "center", verticalalign: "middle", lineheight: "10vh" }}>Broker 1</p>
     </Button>
-    <Button 
+    {/* <Button 
     variant="contained"
     style={{ display: "flex",  width: "25vw", height: "15vh", backgroundColor: "#0857a6", borderRadius: "5%", border: "2px solid black" }}
     >
@@ -23,7 +23,7 @@ export default function Producer(){
     style={{ display: "flex",  width: "25vw", height: "15vh", backgroundColor: "#0857a6", borderRadius: "5%", border: "2px solid black" }}
     >
     <p style={{ color: "white", textalign: "center", verticalalign: "middle", lineheight: "10vh" }}>Broker 3</p>
-    </Button>
+    </Button> */}
     </Stack>
   )
 }
