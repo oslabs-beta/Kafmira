f42ce69bff7afb565b7cf0f61289acc0f1814b25
Gibran Haq <gibran.haq57@gmail.com>
2021-12-27 21:16:01 -0800

    updates for producer/consumer dynamic generation

--- client/components/ClusterContainer.jsx
+++ client/components/ClusterContainer.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Box, Grid } from '@material-ui/core';
 import { Button, Stack } from '@mui/material';
-import Producer from './Producer.jsx';
+//import Producer from './Producer.jsx';
 //import Broker from './Broker.jsx';
-import Consumer from './Consumer.jsx';
+//import Consumer from './Consumer.jsx';
 
 const mapStateToProps = (state) => {
   return {
@@ -28,11 +28,11 @@ function ClusterContainer(props){
       `http://localhost:${props.port}/api/v1/query?query=kafka_producer_producer_metrics_record_send_total`
     ).then((response) => response.json());
 
-    let totalConsumers = fetch(
-      `http://localhost:${props.port}/api/v1/query?query=kafka_consumer_consumer_fetch_manager_metrics_records_consumed_total`
-    ).then((response) => response.json());
+    // let totalConsumers = fetch(
+    //   `http://localhost:${props.port}/api/v1/query?query=kafka_consumer_consumer_fetch_manager_metrics_records_consumed_total`
+    // ).then((response) => response.json());
 
-    Promise.all([totalBrokers, totalProducers, totalConsumers])
+    Promise.all([totalBrokers, totalProducers])
       .then((allData) => {
         //1. set Total Broker Count
         setTotalBrokerCount(allData[0].data.result.length);
@@ -46,25 +46,52 @@ function ClusterContainer(props){
       )},[])
 
   const broker = [];
-  let n = 1;
-  while (n <= totalBrokerCount) {
+  let bCount = 1;
+  while (bCount <= totalBrokerCount) {
     broker.push( <Button
       variant="contained" 
       style={{ display: "flex",  width: "25vw", height: "15vh", backgroundColor: "#0857a6", borderRadius: "5%", border: "2px solid black" }}
       >
-      <p style={{ color: "white", textalign: "center", verticalalign: "middle", lineheight: "10vh" }}>Broker {n}</p>
+      <p style={{ color: "white", textalign: "center", verticalalign: "middle", lineheight: "10vh" }}>Broker {bCount}</p>
       </Button>)
-      n++;
+      bCount++;
   }
 
-  console.log(totalProducerList)
-  console.log(totalConsumerList)
+  const producer = [];
+  let pCount = 0;
+  while (pCount < totalProducerList.length) {
+    if (totalProducerList[0].metric.job === 'producer')
+      producer.push(<Button
+        variant="contained" 
+        style={{ display: "flex",  width: "10vw", height: "10vh", backgroundColor: "#8d39fa", borderRadius: "50%", border: "2px solid black" }}
+        >
+          <p style={{ color: "white", textalign: "center", verticalalign: "middle", lineheight: "10vh" }}>Producer {pCount+1}</p>
+        </Button>)
+        pCount++;
+  }
+
+  const consumer = [];
+  let cCount = 0;
+  while (pCount < totalConsumerList.length) {
+    if (totalConsumerList[0].metric.job === 'consumer') {
+      consumer.push(
+        <Button 
+              style={{ display: 'flex', width: '10vw', height: '10vh', backgroundColor: '#089ba6', borderRadius: '50%', border: "2px solid black"}}
+            >
+              <p style={{color: 'white', textalign: 'center', verticalalign: 'middle', lineheight: '10vh',}}>Consumer {cCount+1}</p>
+          </Button>
+      )
+      cCount++;
+    }
+  }
 
   return(
     <Box sx={{ display: 'flex', justifyContent: 'space-between'}} >
     <Grid container spacing={10}>
       <Grid item>
-        <Producer />
+      <Stack spacing={2}>
+        {producer}
+      </Stack>
       </Grid>
       <Grid item>
         <Stack spacing ={2}>
@@ -72,11 +99,14 @@ function ClusterContainer(props){
         </Stack>
       </Grid>
       <Grid item>
-        <Consumer /> 
+        <Stack spacing={2}>
+        {consumer}
+        </Stack>
      </Grid>
     </Grid>
   </Box>   
   )
+
 }
 
 export default connect(mapStateToProps, null)(ClusterContainer);
