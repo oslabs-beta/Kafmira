90085705b0a53aaca3e6e22efa51428775c61e43
nsofocleous <nsofocleous92@gmail.com>
2022-01-05 15:59:06 -0500

    in process of correcting broker graph data

--- client/components/BrokerGraphContainer.jsx
+++ client/components/BrokerGraphContainer.jsx
@@ -1,25 +1,31 @@
 import React, { useState } from 'react';
 import { Box, Grid, Typography } from '@material-ui/core';
 import BrokerLineGraph from './BrokerLineGraph.jsx'
-import BrokerBarChart from './BrokerBarChart.jsx';
+import BrokerTotalFetchReq from './BrokerTotalFetchReq.jsx'
+import BrokerFailedProdReq from './BrokerFailedProdReq.jsx';
 
 
 
 export default function BrokerGraphContainer(){
   return(
-  <Box sx={{textAlign : 'center'}}>
-    <Typography variant ='h4'>Broker 1</Typography>
-    <Grid
-    container
-    spacing={4}
-    direction="row"
-    justifyContent="center"
-    alignItems="flex-start"
-  >
-    <Grid item xs={4}>
-      <BrokerLineGraph />
-    </Grid>
-  </Grid>
-</Box>
+    <Box sx={{textAlign : 'center'}}>
+      <Typography variant ='h4'>Broker 1</Typography>
+      <Grid
+      container
+      spacing={4}
+      direction="row"
+      justifyContent="center"
+      alignItems="flex-start"
+      >
+        <Grid item xs={4}>
+          <BrokerTotalFetchReq />
+          {/* <BrokerLineGraph /> */}
+        </Grid>
+        {/* <Grid item xs={4}>
+          <BrokerFailedProdReq />
+          <BrokerLineGraph />
+        </Grid> */}
+      </Grid>
+    </Box>
   )
 }
