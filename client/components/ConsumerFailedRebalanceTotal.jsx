a4810547b82adcd9449ae8a59a1d91da7bbb75c9
nsofocleous <nsofocleous92@gmail.com>
2022-01-06 18:24:00 -0500

    edits to component page graphs

--- client/components/ConsumerFailedRebalanceTotal.jsx
+++ client/components/ConsumerFailedRebalanceTotal.jsx
@@ -189,7 +189,8 @@ const ConsumerFailedRebalanceTotal = () => {
     xAxis = obj.data.result[0].values.map(el => {
         let date = new Date(el[0]*1000);
         console.log(el[0]);
-        return date.toLocaleString('en-US');
+        return date.toLocaleDateString('en-US');
+        // return date.toLocaleString('en-US');
     });
 
     obj.data.result.map(el => {
