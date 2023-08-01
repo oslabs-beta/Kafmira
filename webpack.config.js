133f9028dabb0632f003bdc4f5983cb956391fe7
Robert <robert.koulakes@gmail.com>
2023-07-31 19:47:25 -0700

    Cleaned comments and file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,4 @@
 const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-// // Create index.html file that has <script> with src ='/build/bundle.js' injected into it!!!
-// const htmlWebpackPluginConfig = new HtmlWebpackPlugin({
-//   template: "./index.html",
-// });
 
 module.exports = {
   entry: "./client/index.js",
@@ -12,12 +6,9 @@ module.exports = {
     path: path.join(__dirname, "/build"),
     filename: "bundle.js",
   },
-  //mode changed to dev for electron
   mode: "development",
 
   devtool: "inline-source-map",
-
-  //Needed for webpack/electron build
   target: "electron-renderer",
   module: {
     rules: [
@@ -28,15 +19,11 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
-            
-            //@babel/plugin-transform-runtime --> limits unnecessary code duplication as a result of babel's helper transpile functions
-            //must instal @babel/runtime & @babel/plugin-transform-runtime
             plugins: ["@babel/plugin-transform-runtime"]
           },
         },
       },
       {
-        // css-loader interprets @import and url() like import/require() and will resolve them.
         test: /\.css/,
         use: ["style-loader", "css-loader"],
       },
@@ -51,15 +38,3 @@ module.exports = {
     ],
   }
 };
-//   plugins: [htmlWebpackPluginConfig],
-//   devServer: {
-//     static: {
-//       publicPath: "/build/",
-//     },
-//     port: 8080,
-//     proxy: {
-//       "/api": "http://localhost:3000"
-//     },
-//     historyApiFallback: true,
-//   },
-// };
